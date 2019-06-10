b72dce11c1248b83d8a0c8a88fdda05925be2240
Boris Nekezov <boris.nekezov@gmail.com>
2019-06-10 10:43:37 +0300

    Fix code indent

--- src/components/user/UserList.js
+++ src/components/user/UserList.js
@@ -4,49 +4,50 @@ import UserContext from '../../context/UserContext'
 import { GET_USERS } from '../../api/API_URL'
 
 const UserList = () => {
+    
     const [users, setUsers] = useState([]);
     const [filter, setFilter] = useState('');
 
     const currentUser = useContext(UserContext);
     // console.log('currentUser: ', currentUser);
 
- useEffect(() => {
-    async function fetchUsers(){
-        const userResponse = await getUsers();
-        setUsers(userResponse)
-        // console.log('users', users);
-    }
- fetchUsers();   
+    useEffect(() => {
+        async function fetchUsers(){
+            const userResponse = await getUsers();
+            setUsers(userResponse)
+            // console.log('users', users);
+        }
+    fetchUsers();   
 
-    const foundFilter = localStorage.getItem('filter')
+        const foundFilter = localStorage.getItem('filter')
 
-    if(foundFilter){
-        setFilter(foundFilter);
-    }
- }, [])
+        if(foundFilter){
+            setFilter(foundFilter);
+        }
+    }, [])
 
-function changeFilter(value){
-    setFilter(value);
-    localStorage.setItem('filter', value);
-}
-
-function navigate(id){
-    const {permissions} = currentUser;
-
-    if(!permissions.canEdit){
-        return null;
+    function changeFilter(value){
+        setFilter(value);
+        localStorage.setItem('filter', value);
     }
 
-    window.location.href = `/user/${id}`
-}
+    function navigate(id){
+        const {permissions} = currentUser;
 
-//  console.log('users', users);
- // (user.website)
- // ({website}) looks in user
-const filteredUsers = users.filter(({ name }) => name.includes(filter) )
+        if(!permissions.canEdit){
+            return null;
+        }
 
- return (
-    <div>
+        window.location.href = `/user/${id}`
+    }
+
+    //  console.log('users', users);
+    // (user.website)
+    // ({website}) looks in user
+    const filteredUsers = users.filter(({ name }) => name.includes(filter) )
+
+    return (
+        <div>
         <button onClick={() => setFilter('')}>CLEAR</button>
         <button onClick={() => setFilter('.com')}>all with .com</button>
         <button onClick={() => setFilter('.org')}>all with .org</button>
@@ -70,8 +71,8 @@ const filteredUsers = users.filter(({ name }) => name.includes(filter) )
                 </div>
             );
         })}    
-    </div>
- )
+        </div>
+    )
 }
 //  window.location.href = `/user/${id}`
 export default UserList;
